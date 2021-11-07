b433d21968d4b39b604979a87cb596c2c006718a
EkaterinaTrubochkina <33481704+EkaterinaTrubochkina@users.noreply.github.com>
2021-11-07 20:41:32 +0300

    Create HomeWork_10.py

---
+++ HomeWork_10.py
@@ -0,0 +1,13 @@
+# Expand previous Homework 5/6/7/8/9 with additional class, which allow to save records into database:
+# 1.Different types of records require different data tables
+# 2.New record creates new row in data table
+# 3.Implement “no duplicate” check.
+import xml.etree.ElementTree as ET
+import datetime
+import os
+import HomeWork_5_1 as Post
+
+
+
+
+
