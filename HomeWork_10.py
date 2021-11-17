783862804080f99436e7ad47f514c6c763547251
EkaterinaTrubochkina <33481704+EkaterinaTrubochkina@users.noreply.github.com>
2021-11-17 13:56:43 +0300

    draft HW10

--- HomeWork_10.py
+++ HomeWork_10.py
@@ -2,12 +2,60 @@
 # 1.Different types of records require different data tables
 # 2.New record creates new row in data table
 # 3.Implement “no duplicate” check.
-import xml.etree.ElementTree as ET
-import datetime
-import os
-import HomeWork_5_1 as Post
+
+import pyodbc
+# import sqlite3
+# import datetime
+# import os
+# import HomeWork_5_1 as Post
+
+
+class DBConnetion:
+    def __init__(self):
+        self.conn = None
+        self.cur = None
+
+    def connect(self, database_name):
+        self.conn = pyodbc.connect(f'Driver=SQLite3 ODBC Driver;Database={database_name};Direct=True;String Types=Unicode' )
+        self.cur = self.conn.cursor()
+
+    def select(self, field, table):
+        self.cur.execute(f'select {field} from {table}')
+        return self.cur.fetchall()
+
+    def insert(self, table, field1, field2, field3):
+        self.cur.execute(f"""INSERT INTO {table} VALUES ({field1},{field2},{field3})""")
+        self.cur.commit()
+
+    def create_table(self, table, field1, field2, field3):
+        self.cur.execute(f"CREATE TABLE {table} ({field1} varchar(255), {field2} varchar(255), {field3} varchar(255))")
+        self.cur.commit()
+
+    def close_conn(self):
+        self.conn.close()
 
 
 
+# connection = pyodbc.connect('Driver=SQLite3 ODBC Driver;Database=test.db;Direct=True;String Types=Unicode')
+# cursor = connection.cursor()
 
 
+# cursor.execute('CREATE TABLE people (first_name text, last_name text, age real)')
+# cursor.execute("INSERT INTO people VALUES ('Ivanov' , 'Ivan' ,'32')")
+# connection.commit()
+# cursor.execute("select * from people")
+# result = cursor.fetchall()
+# print(result)
+# cursor.close()
+# connection.close()
+
+dbconn= DBConnetion()
+
+dbconn.connect('test2.db')
+# dbconn.create_table('New', 'first_name', 'city', 'data')
+# dbconn.create_table('Ad', 'text', 'data_until', 'days')
+# dbconn.create_table('Recipe', 'text', 'complexity', 'data')
+dbconn.insert('New', 'text', 'city', 'data')
+a = dbconn.select('*', 'New')
+print(a)
+dbconn.close_conn()
--- HomeWork_5_2.py
+++ HomeWork_5_2.py
@@ -13,19 +13,30 @@
 # Each new record should be added to the end of file. Commit file in git for review.
 
 import datetime
-import HomeWork_10 as hm10
 
+class SaveToDB:
+    def write_to_db(self):
+        pass
 
 class Post:
     def __init__(self, title, text):
         self.title = title
         self.text = text
 
-    def publish_article(self, text):
-        with open("Posts.txt", "a") as file:
+    def to_text(self):
+        pass
+
+    def write_to_file(self, filename, text):
+        with open(filename, "a") as file:
             file.write(text)
 
-    def get_data_to_db(self, table, field1, field2, field3):
+    def write_to_sqlite(self, dbname):
+        pass
+
+    def publish_article(self):
+        self.write_to_file("Posts.txt")
+
+    def get_data_to_db(self):
         pass
 
 
@@ -42,13 +53,15 @@ class News(Post):
                f"{35 * '-'}\n"  \
                f"\n\n"
 
-    def get_data_to_db(self, table, field1, field2, field3):
-        db = hm10.DBConnetion('test1.db')
-        try:
-            db.create_table(table, 'text', 'city', 'data')
-        finally:
-            db.insert(table, field1, field2, field3)
+    def get_data_to_db(self):
+        return {
+            "title": self.title,
+            "text": self.text
+        }
 
+    def write_to_file(self, filename, text):
+        with open(filename, "a") as file:
+            file.write(text)
 
 class Ad(Post):
     def __init__(self, title, text, end_date):
@@ -62,13 +75,6 @@ class Ad(Post):
                f"Actual until: {self.end_date}, {days_left.days} days left\n" \
                f"{35 * '-'}\n"  \
                f"\n\n"
-
-    def get_data_to_db(self, table, field1, field2, field3):
-        db = hm10.DBConnetion('test1.db')
-        try:
-            db.create_table(table,'text', 'data_until', 'data')
-        finally:
-            db.insert(table, field1, field2, field3)
 
 
 class Recipe(Post):
@@ -93,26 +99,24 @@ class Recipe(Post):
                f"{35 * '-'}\n"  \
                f"\n\n"
 
-    def get_data_to_db(self, table, field1, field2, field3):
-        db = hm10.DBConnetion('test1.db')
-        try:
-            db.create_table(table, 'text', 'complexity', 'data')
-        finally:
-            db.insert(table, field1, field2, field3)
-
+commands = {
+"1": create_news(),
+"2": create_ad(),
+"3": create_recipe()
+}
 
 def start():
     while True:
         txt = input("Specify type of post:\n1 - News\n2 - Ad \n3 - Recipe\n")
         if txt == "1":
             new_news = News("News", input('Print your text\n'), input("Print your city\n"), datetime.datetime.today())
-            new_news.publish_article(new_news.to_text())
+            new_news.publish_article( 'Posts.txt', new_news.to_text())
         elif txt == "2":
             new_ad = Ad("Ad", input('Print your text\n'), input("Print endDate of ad in dd/mm/yyyy format\n"))
-            new_ad.publish_article(new_ad.to_text())
+            new_ad.publish_article('Posts.txt', new_ad.to_text())
         elif txt == "3":
             new_recipe = Recipe("Recipe of the day", input('Print your recipe\n'), datetime.datetime.today())
-            new_recipe.publish_article(new_recipe.to_text())
+            new_recipe.publish_article('Posts.txt', new_recipe.to_text())
         else:
             print('ERROR')
             exit(0)
@@ -122,5 +126,32 @@ if __name__ == '__main__':
     start()
 
 
+def start():
+    db = SaveToDB("database", "connection", "blabla")
+    while True:
+        txt = input("Specify type of post:\n1 - News\n2 - Ad \n3 - Recipe\n")
+        news = None
+        if txt not in commands:
+            print('ERROR')
+            exit(0)
+        news = commands[txt]()
+        if news is not None:
+            news.publish_article()
+            db.save(news, "tablename")
 
+
+# if txt == "1":
+# news = News("News", input('Print your text\n'), input("Print your city\n"), datetime.datetime.today())
+# elif txt == "2":
+# news = Ad("Ad", input('Print your text\n'), input("Print endDate of ad in dd/mm/yyyy format\n"))
+# elif txt == "3":
+# news = Recipe("Recipe of the day", input('Print your recipe\n'), datetime.datetime.today())
+# else:
+# print('ERROR')
+# exit(0)
+
+
+
+if __name__ == '__main__':
+    start()
 
